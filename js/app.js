6cd8ac06f32ce2802f7f363a82eb38f479f839a9
Shaon2225 <nazim1703073@stud.kuet.ac.bd>
2022-03-02 19:13:55 +0600

    alldone

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let phoneHolder = [];
+
 const phoneContainer = document.getElementById('phone-container');
 const seeMore = document.getElementById('see-more-button');
 const loadingImage = document.getElementById('loading-img');
@@ -6,18 +6,19 @@ const notFound = document.getElementById('not-found');
 let url = '';
 let status = true;
 let noOfPhone=20;
+let phoneHolder = [];
 
 // get search result 
 
 const search = (url,details) =>{
-    loadingImage.style.display = 'block'
     fetch(url)
         .then(res => res.json())
         .then(data => {
             if(details){
-                showPhoneDetails(data);
+                showPhoneDetailInAWindow(data);
             }
             else{
+                loadingImage.style.display = 'block';
                 showPhone(data);
             }
         })
@@ -48,7 +49,7 @@ const showPhone = data =>{
                         <p class="card-text">
                             <h6>Brand : ${phoneHolder[i].brand}</h6>
                         </p>
-                        <a href="#" class="btn btn-primary">Details</a>
+                        <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><button onclick="showPhoneDetail('${phoneHolder[i].slug}')" class ="btn btn-primary">Details</button></a>
                     </div>
                 </div>
             </div>
@@ -84,7 +85,7 @@ const seeMorePhones = ()=>{
                     <p class="card-text">
                         <h6>Brand : ${phoneHolder[i].brand}</h6>
                     </p>
-                    <a href="#" class="btn btn-primary">Details</a>
+                    <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"><button onclick="showPhoneDetail('${phoneHolder[i].slug}')" class ="btn btn-primary">Details</button></a>
                 </div>
             </div>
         </div>
@@ -93,12 +94,48 @@ const seeMorePhones = ()=>{
     }
     seeMore.style.display = 'none';
 } 
-// display phone details
+// making url for phone details
 
-const showPhoneDetails = data =>{
-
+const showPhoneDetail = phoneName =>{
+    url = `https://openapi.programming-hero.com/api/phone/${phoneName}`; 
+    search(url,true);
 }
 
+// display phone details data
+
+const showPhoneDetailInAWindow = data =>{
+    const detailsFeild = document.getElementById('details-container');
+    if (data.status) {
+        let others =Object.entries(data.data.others).map(x => x.join(' : ')).join('<br>');
+        detailsFeild.innerHTML=`
+        <div>
+            <img src="${data.data.image}" width="250px" alt="">
+        </div>
+        <div>
+            <h4 class="fw-bold">Name : ${data.data.name}</h4>
+            <small>${data.data.releaseDate ? data.data.releaseDate : 'Release Date not found'}</small>
+            <h5 class="text-center fw-bold"> Main Features</h5>
+            <ul>
+                <li><span class="fw-bold">Display : </span>${data.data.mainFeatures.displaySize}</li>
+                <li><span class="fw-bold">processor : </span>${data.data.chipSet} </li>
+                <li><span class="fw-bold">memory : </span> ${data.data.memory} </li>
+                <li><span class="fw-bold">storage : </span> ${data.data.storage}</li>
+                <li><span class="fw-bold">sensors : </span>${data.data.mainFeatures.sensors.join(', ')}</li>
+            </ul>
+            <h5 class="text-center fw-bold">Others</h5>
+            <P>${others}</P>
+        </div>
+        `;
+    }
+    else{
+        detailsFeild.innerHTML = `
+        <div id="not-found" class="mx-auto text-center" >
+                <img src="/img/sade-face.svg" alt="">
+                <h2 class="mt-5">Result not found :(</h2>
+        </div>
+        `;
+    }
+}
 
 // getiing input from search field and make a search
 
@@ -106,8 +143,8 @@ document.getElementById('search-btn').addEventListener('click', () =>{
     const inputValue = document.getElementById('input-field').value.toLowerCase();
     url=`https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
     phoneContainer.innerHTML = '';
+    seeMore.style.display='none';
     phoneHolder =[];
-    // phoneContainer.style.display = 'none';
     search(url,false);
 })
 
